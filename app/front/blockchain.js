6337c286a6ebed3ba8082c2c5be7a17871c1c95f
Yessi Munguengui <yessimunguengui@gmail.com>
2020-01-28 16:38:57 +0100

    fix(front): Blockchain Tab responsive

--- app/front/blockchain.js
+++ app/front/blockchain.js
@@ -19,18 +19,36 @@ let blockchainHistory = document.getElementById("blockchainHistory")
 // here we slice the hashs because they are too long for our tab 
 function Stringslice(hash,previousHash) {
   
-  let rawPreviousHash = document.getElementById("previousHash");
-  let rawHash = document.getElementById("hash");
-  
-  // str2.substring(8);
-//str2 = str2.toString().slice(0,3);
-rawHash.value = hash // get the value from the api 
-let hashSliced = rawHash.value.slice(0,4);
-document.getElementById("hash").innerHTML = hashSliced;
-console.log("Ceci est le hash entier "+ rawHash.value);
-console.log("Ceci est le hash découpé "+ hashSliced);
+  var rawPreviousHash = document.getElementById("previousHash");
+  var rawHash = document.getElementById("hash");
+
+
+rawHash.value = hash; // get the value from the api 
+let hashSliced = rawHash.value.slice(0,8);
+document.getElementById("hash").innerHTML = hash;
+
+rawPreviousHash.value = previousHash;
+let previousHashSliced = previousHash.slice(0,8);
+document.getElementById("previousHash").innerHTML = previousHash;
+
+
+// Tests 
+console.log("This is theraw hash"+ rawHash.value);
+console.log("This is the spliced hash"+ hashSliced);
+
+// Tests 
+console.log("This is the raw previous_hash  "+ rawPreviousHash.value);
+console.log("This is the spliced previous_hash "+ previousHashSliced);
+
+
 }
 
-document.onload = Stringslice("Bonjour");
+function showFullPreviousHash(){
+document.all.texte.innerText=rawPrevioushash;
+console.log('Test ecriture');
+}
+
+document.onload = Stringslice("63b74860ef72735b5e3a01a5aff884952484da32c37c0c6c351d8ecaabd18d3d","b10b22134bc01558c79a936b9e5873a881bd9dbf294a242de4430739a52dba3c");
+
 
 
